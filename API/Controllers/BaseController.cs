6d5a43b08228afd8e99b90a5920e683c8e5e1c5c
Gadisa Amenu <amegadbay@gmail.com>
2023-05-18 21:42:34 +0300

    feat(backend.gadisa): initialCommit

---
+++ API/Controllers/BaseController.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BasaApiController : ControllerBase
+    {
+        private IMediator _mediatr;
+
+        protected IMediator Mediator => _mediatr ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        /*    protected ActionResult HandleResult<T>(Result<T> result)
+            {
+                if (result == null)
+                    return NotFound(new Result<int> { Success = false, Message = "Item not found" });
+
+                if (result.Success && result.Value != null)
+                    return Ok(result);
+                if (result.Success && result.Value == null)
+                    return NotFound(result);
+
+
+                return BadRequest(result);
+            }*/
+
+    }
+}
