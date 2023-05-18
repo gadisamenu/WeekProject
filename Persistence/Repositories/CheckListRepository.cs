19ad530b063e3e4c690a1f4d0b303eb385befd0b
Gadisa Amenu <amegadbay@gmail.com>
2023-05-18 21:16:39 +0300

    fix(backend.gadisa): implemented Persistance layer

---
+++ Persistence/Repositories/CheckListRepository.cs
@@ -0,0 +1,28 @@
+using Domain;
+using Application.Contracts.Presistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Repositories
+{
+
+    // ICheckListRepository.cs
+    public class CheckListRepository : Repository<CheckList>,ICheckListRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public CheckListRepository(AppDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        async Task<IEnumerable<CheckList>> ICheckListRepository.GetCheckListsByTaskIdAsync(int taskId)
+        {
+            return await _dbContext.CheckLists.Where(clt => clt.TaskId == taskId).ToListAsync();
+        }
+
+        async Task<IEnumerable<CheckList>> ICheckListRepository.GetCheckListsByCompletionStatusAsync(bool completed)
+        {
+            return await _dbContext.CheckLists.Where(clt => clt.Completed).ToListAsync();
+        }
+    }
+}
---
+++ Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using Application.Contracts.Presistence;
+
+namespace Persistence.Repositories
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _dbContext;
+        private ITaskRepository _taskRepositry;
+        private ICheckListRepository _checkListRepository;
+
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public ITaskRepository TaskRepository
+        {
+            get
+            {
+                if (_taskRepositry == null)
+                    _taskRepositry = new TaskRepository(_dbContext);
+                return _taskRepositry;
+            }
+        }
+        public ICheckListRepository CheckListRepository
+        {
+            get
+            {
+                if (_checkListRepository == null)
+                    _checkListRepository = new CheckListRepository(_dbContext);
+                return _checkListRepository;
+            }
+        }
+
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task<int> Save()
+        {
+            return await _dbContext.SaveChangesAsync();
+        }
+    }
+}
